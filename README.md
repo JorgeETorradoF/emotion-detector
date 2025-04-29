0471ea30bef1a2b7764b0aaf393ea45dc27de4f7
JorgeETorradoF <jorgeetorradoforero@gmail.com>
2025-04-29 18:51:19 -0500

    initial version, maybe we can do more

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pasos para ejecutar
+
+## 1. En su carpeta donde esté el .py cree un entorno virtual (preferible, no obligatorio):
+```bash
+    python -m venv <nombre de su entorno virtual (no use tildes ni espacios de preferencia, dejelo suave)>
+```
+## 2. Cambie al entorno virtual:
+```bash
+    <nombre de su entorno virtual>\Scripts\activate
+```
+
+## 3. Instale:
+```bash
+pip install deepface opencv-python tensorflow keras
+pip install tf-keras
+pip install ultralytics
+```
+## 4. Ejecute:
+```bash
+    python emotionDetection.py
+```
---
+++ emotionDetection.py
@@ -0,0 +1,47 @@
+from deepface import DeepFace
+import cv2
+import numpy as np
+
+def obtener_emocion_dominante(result):
+    return result[0]['dominant_emotion']
+
+# Iniciar cámara
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("No se pudo acceder a la cámara")
+else:
+    print("Presiona 'q' para salir")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    try:
+        # Analizar emoción en el frame actual
+        result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False, detector_backend='yolov8',align=True)
+
+        # Obtener coordenadas del rostro y emoción dominante
+        face_region = result[0]['region']
+        x, y, w, h = face_region['x'], face_region['y'], face_region['w'], face_region['h']
+        emocion = obtener_emocion_dominante(result)
+
+        # Dibujar rectángulo (verde)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Dibujar el texto con la emoción (ajustar posición si es necesario)
+        cv2.putText(frame, emocion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+
+    except Exception as e:
+        print("Error:", e)
+
+    # Mostrar frame
+    cv2.imshow('Detección de Emociones', frame)
+
+    # Salir con 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
